b3e3b9e17fb5112bd5e7d89adebe54545bbc6c30
‘Rustam’ <rustamjonisroilov213@gmail.com>
2024-09-10 16:08:57 +0500

    userslarni bo'lib chiqarish

--- handlers/users/useslar.py
+++ handlers/users/useslar.py
@@ -21,9 +21,11 @@ async def all_users(message: types.Message):
         text += f"Id: {i['id']}\n"
         text += f"🧑‍💼Name: {i['name']}\n"
         text += f"🆔Chat id: {i['chat_id']}\n"
-
+        if i["id"] % 10 == 5:
+            await message.answer(text, reply_markup=delete_text)
+            text = f"Users: {len(users.json())}\n"
     # print(users[0][0])
-    await message.answer(text, reply_markup=delete_text)
+    # await message.answer(text, reply_markup=delete_text)
 
 
 @dp.message_handler(text='📲 Reklama', user_id=ADMINS)
