40e63dcc6f3b48f20c1d257d06956e4f09ef1e92
‘Rustam’ <rustamjonisroilov213@gmail.com>
2024-09-10 16:26:30 +0500

    userslarni bo'lib chiqarish 40

--- handlers/users/useslar.py
+++ handlers/users/useslar.py
@@ -24,6 +24,7 @@ async def all_users(message: types.Message):
         if i["id"] % 40 == 0:
             await message.answer(text, reply_markup=delete_text)
             text = f"Users: {len(users.json())}\n"
+
     await message.answer(text, reply_markup=delete_text)
 
 
